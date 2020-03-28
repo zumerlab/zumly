f68115df3038b65b7a70c1e283ad9295b2a1eeb8
zircleUI <juan.martin.muda@gmail.com>
2020-03-28 14:16:42 -0300

    fix: 🐛 Reorder layers
    Layers were bad allocated. Inversion was made

--- src/zumion.js
+++ src/zumion.js
@@ -1,14 +1,14 @@
 export default class Zumly {
-  constructor (options) {
+constructor(options) {
     this.app = options
     this.storedViews = []
-  }
-  init () {
+}
+init() {
     const rootDiv = document.querySelector(this.app.mount)
     var self = this
-    rootDiv.addEventListener('click', function (e) {
-      e.stopPropagation()
-      self.zoomOut()
+    rootDiv.addEventListener('click', function(e) {
+        e.stopPropagation()
+        self.zoomOut()
     })
     var newView = document.createElement('template')
     newView.innerHTML = this.app.views[this.app.initialView]
@@ -21,32 +21,30 @@ export default class Zumly {
     this.storeViews({
         zoomLevel: this.storedViews.length,
         views: [
-      // se guardan datos previos
-          { 
-            location: 'current',
-            viewName: this.app.initialView,
-            backwardState: {
-              origin: view.style.transformOrigin,
-              transition: view.style.transition,
-              transform: view.style.transform
-            },
-            forwardState: null
-          }
+            // se guardan datos previos
+            {
+                location: 'current',
+                viewName: this.app.initialView,
+                backwardState: {
+                    origin: view.style.transformOrigin,
+                    transition: view.style.transition,
+                    transform: view.style.transform
+                },
+                forwardState: null
+            }
         ]
     })
     // agrega eventos a todos los .zoomable
     var self = this
-    view.querySelectorAll('.zoomable')
-      .forEach(el => el.addEventListener('click', function (e) {
+    view.querySelectorAll('.zoomable').forEach(el => el.addEventListener('click', function(e) {
         e.stopPropagation()
         self.zoomIn(el)
-      }))
-  }
-  storeViews (data) {
+    }))
+}
+storeViews(data) {
     this.storedViews.push(data)
-    // console.log(this.storedViews)
-  }
-  zoomOut () {
+}
+zoomOut() {
     if (this.storedViews.length > 1) {
         var ultimaVista = this.storedViews[this.storedViews.length - 1]
         let current = ultimaVista.views[0]
@@ -55,6 +53,8 @@ export default class Zumly {
         let gone = ultimaVista.views[3]
         const canvas = document.querySelector(this.app.mount)
         var currentView = canvas.querySelector('.view.current')
+        currentView.classList.remove('current')
+        currentView.classList.add('disappear')
         var previousView = canvas.querySelector('.view.previous')
         var lastView = canvas.querySelector('.view.last')
         //console.log(currentView, previousView)
@@ -62,42 +62,33 @@ export default class Zumly {
         currentView.style.transition = current.backwardState.transition
         currentView.style.transform = current.backwardState.transform
         currentView.addEventListener('transitionend', () => {
-          canvas.removeChild(canvas.querySelector('.view.current'))
-          })
+            canvas.removeChild(canvas.querySelector('.view.disappear'))
+        })
         let elementoClickeado = previousView.querySelector('.active')
         elementoClickeado.classList.remove('active')
-        previousView.style.filter = ''
+        // previousView.style.filter = ''
         previousView.classList.remove('previous')
         previousView.classList.add('current')
         previousView.style.transformOrigin = previous.backwardState.origin
         previousView.style.transition = previous.backwardState.transition
-        previousView.style.transform =previous.backwardState.transform
+        previousView.style.transform = previous.backwardState.transform
         if (last !== undefined) {
-          lastView.classList.remove('last')
-          lastView.classList.add('previous')
-          lastView.style.opacity = 1 // hack opacity
-          lastView.style.transformOrigin = last.backwardState.origin
-          lastView.style.transition = last.backwardState.transition
-          lastView.style.transform =last.backwardState.transform
-          // canvas.removeChild(canvas.querySelector('div:nth-child(1)'))
+            lastView.classList.remove('last')
+            lastView.classList.add('previous')
+            lastView.style.opacity = 1
+            lastView.style.transformOrigin = last.backwardState.origin
+            lastView.style.transition = last.backwardState.transition
+            lastView.style.transform = last.backwardState.transform
         }
         if (gone !== undefined) {
-          //var newView = document.createElement('template')
-          //newView.innerHTML = gone.viewName
-          canvas.append(gone.viewName)
-          var newlastView = canvas.querySelector('.view:last-child')
-          newlastView.style.opacity = 0 // hack opacity
-          //newlastView.classList.add('last')
-          //newlastView.style.transformOrigin = gone.backwardState.origin
-          //newlastView.style.transition = gone.backwardState.transition
-          //newlastView.style.transform = gone.backwardState.transform
+            canvas.prepend(gone.viewName)
+            var newlastView = canvas.querySelector('.view:last-child')
+            newlastView.style.opacity = 0
         }
         this.storedViews.pop()
     }
-    
-    //FALTA INCORPORAR VIEWS SACADAS Y CAMBIAR LAS CLASES 
-  }
-  zoomIn (el) {
+}
+zoomIn(el) {
     // clicked element with .zoomable
     el.classList.add('active')
     let coordenadasEl = el.getBoundingClientRect()
@@ -106,10 +97,13 @@ export default class Zumly {
     // create new view
     var newView = document.createElement('template')
     newView.innerHTML = this.app.views[el.dataset.goTo]
+    // create overlay effect
+    //var overlay = document.createElement('template')
+    //overlay.innerHTML = `<div class='overlay'></div>`
     // transforma current view en previous view
     var previousView = document.querySelector('.current')
     let coordenadasPreviousView = previousView.getBoundingClientRect()
-    var lastView = null 
+    var lastView = null
     var goneView = null
     goneView = document.querySelector('.last')
     if (goneView !== null) {
@@ -118,10 +112,10 @@ export default class Zumly {
     // si existe transforma previous view en last view
     lastView = document.querySelector('.previous')
     if (lastView !== null) {
-      lastView.classList.remove('previous')
-      lastView.classList.add('last')
-      var coordenadasLastView = lastView.getBoundingClientRect()
-      var clvt = lastView.style.transform
+        lastView.classList.remove('previous')
+        lastView.classList.add('last')
+        var coordenadasLastView = lastView.getBoundingClientRect()
+        var clvt = lastView.style.transform
     }
     //
     previousView.classList.add('previous')
@@ -129,31 +123,33 @@ export default class Zumly {
     // transforma new view en current view y la agrega al DOM al ppio del container
     newView.classList.add('current')
     var mountPoint = document.querySelector(this.app.mount)
-    mountPoint.prepend(newView.content)
-    document.querySelector('.view').classList.add('current')
+    //mountPoint.prepend(overlay.content)
+    mountPoint.append(newView.content)
+    mountPoint.querySelector('.view:last-child').classList.add('current')
     var currentView = document.querySelector('.current')
+    currentView.classList.add('appear')
     currentView.dataset.viewName = el.dataset.goTo
     currentView.classList.add('no-events')
     let coordenadasCurrentView = currentView.getBoundingClientRect()
     // agrega eventos al currentview
     var self = this
-    currentView.querySelectorAll('.zoomable')
-      .forEach(vx => vx.addEventListener('click', function (e) {
+    currentView.querySelectorAll('.zoomable').forEach(vx => vx.addEventListener('click', function(e) {
         e.stopPropagation()
         self.zoomIn(vx)
-      })) // HANBRIA QUE REMOVCER EL EVENTO AL REMOVER LA VIEW
-    currentView.onclick = function (e) {
-      e.stopPropagation()
-      console.log('ff')
+    }))
+    currentView.onclick = function(e) {
+        e.stopPropagation()
     }
     // canvas
     let scale = coordenadasCurrentView.width / coordenadasEl.width
     let scaleInv = 1 / scale
+    let scaley = coordenadasCurrentView.height / coordenadasEl.height
+    let scaleInvy = 1 / scaley
     previousView.style.transition = 'transform 0s'
     previousView.style.transformOrigin = `
-    ${coordenadasEl.x + coordenadasEl.width / 2 - coordenadasPreviousView.x}px
-    ${coordenadasEl.y + coordenadasEl.height / 2 - coordenadasPreviousView.y}px
-    `
+${coordenadasEl.x + coordenadasEl.width / 2 - coordenadasPreviousView.x}px
+${coordenadasEl.y + coordenadasEl.height / 2 - coordenadasPreviousView.y}px
+`
     let x = coordenadasCanvas.width / 2 - coordenadasEl.width / 2 - coordenadasEl.x + coordenadasPreviousView.x
     let y = coordenadasCanvas.height / 2 - coordenadasEl.height / 2 - coordenadasEl.y + coordenadasPreviousView.y
     let move = `translate3d(${x}px, ${y}px, 0px) scale(${scale})`
@@ -162,124 +158,136 @@ export default class Zumly {
     // vuelve a setear coordenadas
     if (lastView !== null) {
         var newcoordenadasPV = previousView.getBoundingClientRect()
-        // var prevEl = document.querySelector('.previous > .zoomable')
-        //var cpe = prevEl.getBoundingClientRect()
         lastView.style.transition = 'transform 0s'
-        // var scale1 = coordenadasLastView.width / cpe.width
         lastView.style.transform = `translate3d(${x}px, ${y}px, 0px) scale(${scale * scale})`
-        var last = document.querySelector('.last > .active') // BUG A RESOLVER... CUANDO SE VUELVE A METER LA VIEW...
-        // QUIZAS LA SOLUCION ES METER LA VIEW ENTERA EN GONE... :)
+        var last = document.querySelector('.last > .active')
         var coorLast = last.getBoundingClientRect()
         lastView.style.transform = clvt
     }
     previousView.style.transform = `translate3d(${coordenadasPreviousView.x}px, ${coordenadasPreviousView.y}px, 0px)`
     var prePrevTransform = previousView.style.transform
     el.getBoundingClientRect()
-    // previousView.style.filter = 'blur(2px)'
     previousView.style.transition = `transform ${scale * 0.4}s ease-in-out`
-    // requestAnimationFrame(function() {
-        previousView.style.transform = move
-    // })
-    ///
+    previousView.style.transform = move
     currentView.style.transformOrigin = 'top left'
-    // requestAnimationFrame(function() {
-        currentView.style.transform = `translate3d(${coordenadasEl.x}px, ${coordenadasEl.y}px, 0px) scale(${scaleInv})`
-        var preCurrentTransform = currentView.style.transform
-    // })
+    currentView.style.transform = `translate3d(${coordenadasEl.x}px, ${coordenadasEl.y}px, 0px) scale(${scaleInv})`
+    var preCurrentTransform = currentView.style.transform
     if (lastView !== null) {
         lastView.style.transition = `transform ${scale * 0.4}s ease-in-out`
         var prev = document.querySelector('.previous')
         var coorPrev = prev.getBoundingClientRect()
-        // requestAnimationFrame(function() {
-            lastView.style.transform = `translate3d(${coordenadasCanvas.width / 2 - coordenadasEl.width / 2 - coordenadasEl.x + (coorPrev.x - coorLast.x) + newcoordenadasPV.x}px, ${coordenadasCanvas.height / 2 - coordenadasEl.height / 2 - coordenadasEl.y + (coorPrev.y - coorLast.y) + newcoordenadasPV.y}px, 0px) scale(${scale * scale})`
-        // })
-    } 
+        lastView.style.transform = `translate3d(${coordenadasCanvas.width / 2 - coordenadasEl.width / 2 - coordenadasEl.x + (coorPrev.x - coorLast.x) + newcoordenadasPV.x}px, ${coordenadasCanvas.height / 2 - coordenadasEl.height / 2 - coordenadasEl.y + (coorPrev.y - coorLast.y) + newcoordenadasPV.y}px, 0px) scale(${scale * scale})`
+    }
     el.getBoundingClientRect()
     currentView.style.transition = `transform ${scale * 0.4}s ease-in-out`
-    // requestAnimationFrame(function() {
-        currentView.style.transform = `translate3d(${newcoordenadasEl.x}px, ${newcoordenadasEl.y}px, 0px)`
-        currentView.addEventListener('transitionend', () => currentView.classList.remove('no-events'))
-    // })
-
-    // ADD GONE VIIEW Y LISTO EL POLLO
+    currentView.style.transform = `translate3d(${newcoordenadasEl.x}px, ${newcoordenadasEl.y}px, 0px)`
+    currentView.addEventListener('transitionend', () => {
+      currentView.classList.remove('no-events')
+      currentView.classList.remove('appear')
+    })
     var snapShoot = {
-      zoomLevel: this.storedViews.length,
-      views: []
+        zoomLevel: this.storedViews.length,
+        views: []
     }
-    let currentv = currentView ? 
-          { 
-            location: 'current',
-            viewName: currentView.dataset.viewName,
-            backwardState: {
-              origin: currentView.style.transformOrigin,
-              transition: currentView.style.transition,
-              transform: preCurrentTransform
-            },
-            forwardState: {
-              origin: currentView.style.transformOrigin,
-              transition: currentView.style.transition,
-              transform: currentView.style.transform
-            }
-          } : null
-    let previousv = previousView ? 
-          {
-            location: 'previous',
-            viewName: previousView.dataset.viewName, // dsp poner nombre
-            backwardState: {
-              origin: previousView.style.transformOrigin,
-              transition: previousView.style.transition,
-              transform: prePrevTransform
-            },
-            forwardState: {
-              origin: previousView.style.transformOrigin,
-              transition: previousView.style.transition,
-              transform: previousView.style.transform
-            }
-          } : null
-    let lastv = lastView ? 
-          { 
-            location: 'last',
-            viewName: lastView.dataset.viewName, // dsp poner nombre
-            backwardState: {
-              origin: lastView.style.transformOrigin,
-              transition: lastView.style.transition,
-              transform: clvt
-            },
-            forwardState: {
-              origin: lastView.style.transformOrigin,
-              transition: lastView.style.transition,
-              transform: lastView.style.transform
-            }
-          } : null
-    let gonev = goneView ? 
-          { // ACA VA LA VISTA ENTERA FALTA REALIZAR UN ZOOM IGUAL ANTES DE SACARLA DE JUEGO
-            location: 'gone',
-            viewName: goneView, //.dataset.viewName, // dsp poner nombre
-            backwardState: {
-              origin: goneView.style.transformOrigin,
-              transition: goneView.style.transition,
-              transform: goneView.style.transform
-            },
-            forwardState: null
-          } : null
+    let currentv = currentView ? {
+        location: 'current',
+        viewName: currentView.dataset.viewName,
+        backwardState: {
+            origin: currentView.style.transformOrigin,
+            transition: currentView.style.transition,
+            transform: preCurrentTransform
+        },
+        forwardState: {
+            origin: currentView.style.transformOrigin,
+            transition: currentView.style.transition,
+            transform: currentView.style.transform
+        }
+    } : null
+    let previousv = previousView ? {
+        location: 'previous',
+        viewName: previousView.dataset.viewName,
+        backwardState: {
+            origin: previousView.style.transformOrigin,
+            transition: previousView.style.transition,
+            transform: prePrevTransform
+        },
+        forwardState: {
+            origin: previousView.style.transformOrigin,
+            transition: previousView.style.transition,
+            transform: previousView.style.transform
+        }
+    } : null
+    let lastv = lastView ? {
+        location: 'last',
+        viewName: lastView.dataset.viewName,
+        backwardState: {
+            origin: lastView.style.transformOrigin,
+            transition: lastView.style.transition,
+            transform: clvt
+        },
+        forwardState: {
+            origin: lastView.style.transformOrigin,
+            transition: lastView.style.transition,
+            transform: lastView.style.transform
+        }
+    } : null
+    let gonev = goneView ? { // ACA VA LA VISTA ENTERA FALTA REALIZAR UN ZOOM IGUAL ANTES DE SACARLA DE JUEGO
+        location: 'gone',
+        viewName: goneView,
+        backwardState: {
+            origin: goneView.style.transformOrigin,
+            transition: goneView.style.transition,
+            transform: goneView.style.transform
+        },
+        forwardState: null
+    } : null
     if (currentv !== null) snapShoot.views.push(currentv)
-      if (previousv !== null) snapShoot.views.push(previousv)
-        if (lastv !== null) snapShoot.views.push(lastv)
-          if (gonev !== null) snapShoot.views.push(gonev)
+    if (previousv !== null) snapShoot.views.push(previousv)
+    if (lastv !== null) snapShoot.views.push(lastv)
+    if (gonev !== null) snapShoot.views.push(gonev)
     this.storeViews(snapShoot)
+}
+}
+/*(function() {
+  var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
+                              window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+  window.requestAnimationFrame = requestAnimationFrame;
+})();
+
+var start = null;
+var element = document.getElementById('SomeElementYouWantToAnimate');
+
+function step(timestamp) {
+  if (!start) start = timestamp;
+  var progress = timestamp - start;
+  element.style.transform = 'translateX(' + Math.min(progress / 10, 200) + 'px)';
+  if (progress < 2000) {
+    window.requestAnimationFrame(step);
   }
 }
+
+window.requestAnimationFrame(step);*/
 // TEMAS A RESOLVER:
-// 
+// CAMBIAR ORDEN LAYERS ESTAN INVERTIDOS.
+// crear un layer blur o de diferrente estilos para no afectar el rendimiento de la navegacion
+// usando backdrop-filter. no anda no ffox
+// limitar scale factor en altura? limitar tamanos de los zoomable y views?
+// PASAR A ANIMATINS CSS CON CSS VARIABLES
+// ver tema de transicion de la nueva vista in and out
+// ver bus de ejecucion de transition aun en movimiento
+// poner opciones para los devs: efectos blur, velocidad variable, constante, custom de transicion
+// ultra optimizar el zoomin, zoomout...
+// // Set will-change when the element is hovered
+/*el.addEventListener('mouseenter', hintBrowser);
+el.addEventListener('animationEnd', removeHint);
+
+function hintBrowser() {
+  // The optimizable properties that are going to change
+  // in the animation's keyframes block
+  this.style.willChange = 'transform, opacity';
+}
+
+function removeHint() {
+  this.style.willChange = 'auto';
+}*/
 // - dsp usar css vars
-// - LISTO DESHABILITAR CLICK ON TRANSITIONS VIA JS O CSS. VIA CSS. FALTA DESHABILITAR LA NEW CURRENT VIEW VIA UNA CLASS TEMPORAIA
-// LISTO AUMENTAR SCALE DE LASTVIEW. 
-// - CAMBIAR EL NOMBRE ADEMAS lasrvoew
-// WIP ZOOM BACK: - se hace por dataset.viewName
-// - USR FLIP (PODRIA ANDAR)
-// LISTO ver tema de centrar new view en duferebte elementos
-// LISTO identificar zoombale el clickeado
-// LISTO tener en cuenta posicion inicial de las vistas, que afecta como se renderizan en la nueva vista. quizas hacer un overrride de esas cosas en caso que no sea la vista inicial.
-// LISTO ver tema de tamano diferente de las .views
-// LISTO zoom infinito YEAHH!!
-// LISTO el resultado de previus view aka last se resta al resultado freal de prevuous
