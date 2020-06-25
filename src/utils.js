5bc1580e37451cba8a054705a950a7811c9f5f8d
zircleUI <juan.martin.muda@gmail.com>
2020-06-25 12:17:41 -0300

    refactor: 💡 Improve notifications

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,6 @@
-/* Utils */
+/** 
+ * utils.js: a set of useful functions for Zumly
+ */
 export function shimIdleCallBack (cb) {
   var start = Date.now();
   return setTimeout(() => {
@@ -12,43 +14,43 @@ export function prepareCSS (instance) {
   let result = ''
   views.map(view => {
   result += `
-.zoom-${view}-${instance} {
-  -webkit-animation-name: zoom-${view}-${instance};
-          animation-name: zoom-${view}-${instance};
-  -webkit-animation-duration: var(--zoom-duration-${instance});
-          animation-duration: var(--zoom-duration-${instance});
-  -webkit-animation-timing-function: var(--zoom-ease-${instance});
-          animation-timing-function: var(--zoom-ease-${instance});
-}
-@-webkit-keyframes zoom-${view}-${instance} {
-  0% {
-    transform-origin: var(--${view}-transformOrigin-start-${instance});
-    transform: var(--${view}-transform-start-${instance});
-    opacity: var(--${view}-opacity-start-${instance});
-    filter: var(--${view}-filter-start-${instance})
+  .zoom-${view}-${instance} {
+    -webkit-animation-name: zoom-${view}-${instance};
+            animation-name: zoom-${view}-${instance};
+    -webkit-animation-duration: var(--zoom-duration-${instance});
+            animation-duration: var(--zoom-duration-${instance});
+    -webkit-animation-timing-function: var(--zoom-ease-${instance});
+            animation-timing-function: var(--zoom-ease-${instance});
   }
-  100% {
-    transform-origin: var(--${view}-transformOrigin-end-${instance});
-    transform: var(--${view}-transform-end-${instance});
-    opacity: var(--${view}-opacity-end-${instance});
-    filter: var(--${view}-filter-end-${instance})
+  @-webkit-keyframes zoom-${view}-${instance} {
+    0% {
+      transform-origin: var(--${view}-transformOrigin-start-${instance});
+      transform: var(--${view}-transform-start-${instance});
+      opacity: var(--${view}-opacity-start-${instance});
+      filter: var(--${view}-filter-start-${instance})
+    }
+    100% {
+      transform-origin: var(--${view}-transformOrigin-end-${instance});
+      transform: var(--${view}-transform-end-${instance});
+      opacity: var(--${view}-opacity-end-${instance});
+      filter: var(--${view}-filter-end-${instance})
+    }
   }
-}
-@keyframes zoom-${view}-${instance} {
-  0% {
-    transform-origin: var(--${view}-transformOrigin-start-${instance});
-    transform: var(--${view}-transform-start-${instance});
-    opacity: var(--${view}-opacity-start-${instance});
-    filter: var(--${view}-filter-start-${instance})
+  @keyframes zoom-${view}-${instance} {
+    0% {
+      transform-origin: var(--${view}-transformOrigin-start-${instance});
+      transform: var(--${view}-transform-start-${instance});
+      opacity: var(--${view}-opacity-start-${instance});
+      filter: var(--${view}-filter-start-${instance})
+    }
+    100% {
+      transform-origin: var(--${view}-transformOrigin-end-${instance});
+      transform: var(--${view}-transform-end-${instance});
+      opacity: var(--${view}-opacity-end-${instance});
+      filter: var(--${view}-filter-end-${instance})
+    }
   }
-  100% {
-    transform-origin: var(--${view}-transformOrigin-end-${instance});
-    transform: var(--${view}-transform-end-${instance});
-    opacity: var(--${view}-opacity-end-${instance});
-    filter: var(--${view}-filter-end-${instance})
-  }
-}
-`})
+  `})
   instanceStyle.innerHTML = result
   document.head.appendChild(instanceStyle)
 }
@@ -98,3 +100,43 @@ export function setCSSVariables (transition, currentStage, instance) {
     }
   })
 }
+
+export async function renderView (el, canvas, views, init) {
+  return new Promise((resolve) => {
+    var viewName = null
+    init ? viewName = el : viewName = el.dataset.to 
+    requestIdleCallback(async () => {
+      var newView = document.createElement('template')
+      newView.innerHTML = await views[viewName].render()
+      let vv = newView.content.querySelector('.z-view')
+      if (!init) {
+        vv.classList.add('is-new-current-view')
+        vv.classList.add('has-no-events')
+        vv.classList.add('hide')
+        vv.classList.add('performance')
+      } else {
+        vv.classList.add('is-current-view')
+      }
+      vv.style.transformOrigin = `0 0`
+      vv.dataset.viewName = viewName
+      var rect = canvas.append(newView.content)
+      resolve(rect)
+    })
+  })
+}
+
+export function notification (debug, msg, type) {
+  if (msg && type === 'welcome') {
+    console.info(`%c Zumly %c ${msg}`, `background: #424085; color: white; border-radius: 3px;`, `color: #424085`) // eslint-disable-line no-console
+  }
+  if (msg && debug && type === 'info' || type === undefined) {
+    console.info(`%c Zumly %c ${msg}`, `background: #6679A3; color: #304157; border-radius: 3px;`, `color: #6679A3`) // eslint-disable-line no-console
+  }
+  if (msg && type === 'warn') {
+    console.warn(`%c Zumly %c ${msg}`, `background: #DCBF53; color: #424085; border-radius: 3px;`, `color: #424085`) // eslint-disable-line no-console
+  }
+   if (msg && type === 'error') {
+    console.error(`%c Zumly %c ${msg}`, `background: #BE4747; color: white; border-radius: 3px;`, `color: #424085`) // eslint-disable-line no-console
+  }
+}
+
