e441f9bd54f79ff2ea922fa80ba52de17c5dfe81
zircleUI <juan.martin.muda@gmail.com>
2020-07-15 20:15:09 -0300

    test: 💍 add unit tests

---
+++ __tests__/methods.test.js
@@ -0,0 +1,99 @@
+/* eslint-env jest */
+import Zumly from '../src/zumly.js'
+// import { renderView } from '../src/utils.js'
+import {jest} from '@jest/globals';
+
+let homeView
+let appMocked
+
+beforeEach( async () => {
+// prepare template for testing
+  document.body.innerHTML = `<!-- DIV canvas -->
+    <div class="first zumly-canvas"></div>
+   `;
+
+  homeView = `<div class="z-view">
+    <div class='card zoom-me' data-to='newView'></div>
+  </div>`
+
+  appMocked = new Zumly({
+        mount: '.first',
+        initialView: 'homeView',
+        views: {homeView}
+      })
+  // not start zumly
+  // await appMocked.init()
+
+})
+
+describe('Zumly methods', () => {
+  it('storeViews() should store views and notify', () => {
+    appMocked.tracing = jest.fn();
+    appMocked.storeViews('test')
+    expect(appMocked.tracing).toHaveBeenCalledWith('storedViews()');
+    expect(appMocked.storedViews).toEqual(['test']);
+  })
+
+  it('setPreviousScale() should store scale and notify', () => {
+    appMocked.tracing = jest.fn();
+    expect(appMocked.storedPreviousScale.length).toEqual(1);
+    appMocked.setPreviousScale(1)
+    expect(appMocked.tracing).toHaveBeenCalledWith('setPreviousScale()');
+    expect(appMocked.storedPreviousScale.length).toEqual(2);
+  })
+
+  it('tracing() should increment trace or reset it', () => {
+    appMocked.tracing('ended')
+    expect(appMocked.trace.length).toEqual(0);
+    appMocked.tracing('test')
+    expect(appMocked.trace.length).toEqual(1);
+  })
+
+  it('counter() should increment when it is called', () => {
+    Zumly._counter = null
+    expect(Zumly.counter).toEqual(1);
+    expect(Zumly.counter).toEqual(2);
+  })
+
+  it('notify() should return storedViews.length', () => {
+    // check this test
+    expect(appMocked.notify('alert', 'error')).toBe(undefined);
+  })
+
+  it('zoomLevel() should return storedViews.length', () => {
+    appMocked.storedViews = [1, 1, 1, 1, 1, 1]
+    expect(appMocked.zoomLevel()).toBe(6);
+  })
+
+  it('init() should call methods', async () => {
+    appMocked.tracing = jest.fn();
+    // appMocked.notify = jest.fn();
+    appMocked.init()
+    // appMocked.options = true
+    expect(appMocked.tracing).toHaveBeenCalledWith('init()');
+    //expect(appMocked.notify).toHaveBeenCalled();
+    
+  })
+
+  it('zoomIn(el) should call methods', async () => {
+    appMocked.tracing = jest.fn();
+    // appMocked.notify = jest.fn();
+    appMocked.zoomIn()
+    // appMocked.options = true
+    expect(appMocked.tracing).toHaveBeenCalledWith('zoomIn()');
+    expect(appMocked.tracing).toHaveBeenCalledWith('renderView()');
+    //expect(appMocked.notify).toHaveBeenCalled();
+    
+  })
+
+  it('zoomOut() should call methods', async () => {
+    // appMocked.tracing = jest.fn();
+   // appMocked.zoomOut()
+    // appMocked.options = true
+    //expect(appMocked.tracing).toHaveBeenCalledWith('zoomOut()');
+    
+  })
+  
+  
+})
+
--- __tests__/zumly.test.js
+++ __tests__/zumly.test.js
@@ -1,62 +1,156 @@
 /* eslint-env jest */
 /*
+https://github.com/mawrkus/js-unit-testing-guide
 Here's how a typical test flow looks like:
 -import the function to test
 -give an input to the function
 -define what to expect as the output
 -check if the function produces the expected output
+
+testear metodos de zumly
+
+testear simulacion de interaccion (ver tobecalled)
+
+testear util functions
+
+
+Test simple user actions
+Example of simple user actions:
+
+Clicking on a link that toggles the visibility of a DOM element
+Submitting a form that triggers the form validation
+(...)
+
+Note how simple the test is because the UI (DOM) layer does not mix with the business logic layer:
+
+a "click" event occurs
+a public method is called
+The next step could be to test the business logic implemented in "showPreview()" or "hidePreview()".
+
 */
 import Zumly from '../src/zumly.js'
-beforeEach(() => {
-  // prepare template for testing
-  document.body.innerHTML = `
-    <!-- DIV canvas -->
-    <div class="first zumly-canvas"></div>
-    <div class="second zumly-canvas"></div>
-  `;
+import {jest} from '@jest/globals';
+// some variables for testing purposes
+let zumly0
+let zumly1
+let homeView
 
+
+beforeAll(async () => {
+  // prepare template for testing
+  document.body.innerHTML = `<!-- DIV canvas -->
+    <div class="first zumly-canvas"></div>
+    <div class="second zumly-canvas"></div>`;
+
+  homeView = `<div class="z-view">
+    <div class='card zoom-me' data-to='newView'></div>
+  </div>`
+      
+  // prepare zumly instances for testing
+  zumly0 = new Zumly({
+        mount: '.first',
+        initialView: 'homeView',
+        views: {homeView}
+      })
+  await zumly0.init()
+  
+  zumly1 = new Zumly({
+        mount: '.second',
+        initialView: 'homeView',
+        views: {homeView},
+        transitions: {
+          effects: ['blur','sepia','saturate']
+        }
+      })
+     await zumly1.init()
 });
 
+describe('Zumly parameters', () => {
 
-describe('Zumly class', () => {
-  const home = `
-  <div class="z-view">
-   <div class='card zoom-me' data-to='newView'></div>
-  </div>`
-  it('Zumly({options})', async () => {
+  describe('that are required', () => {
 
-    const app = new Zumly({
-              mount: '.first',
-              initialView: 'home',
-              views: {home},
-              transitions: {
-                effects: ['blur','sepia','saturate']
-              }
-            })
-    await app.init()
+    it('mount should be a string', () => {
 
-    const app1 = new Zumly({
-              mount: '.second',
-              initialView: 'home',
-              views: {home}
-            })
-    await app1.init()
-    // instances
-    expect(app.instance).toBe(1)
-    expect(app1.instance).toBe(2)
-    expect(document.body).toMatchSnapshot()
-    // properties required
-    expect(app.mount).toBe('.first')
-    expect(app.initialView).toBe('home')
-    expect(app.views).toMatchObject({home})
-    // properties default
-    expect(app.cover).toBe('width')
-    expect(app.duration).toBe('1s')
-    expect(app.ease).toBe('ease-in-out')
-    await expect(app1.effects).toEqual(expect.arrayContaining(['none','none']))
-    await expect(app.effects).toEqual(expect.arrayContaining(['blur(0px) sepia(0) saturate(0) ', 'blur(0.8px) sepia(5) saturate(8) ']))
-    expect(app.debug).toBe(false)
+      expect(zumly0.mount).toBe('.first')
 
+    })
+
+    it('initialView should be a string', () => {
+
+      expect(zumly0.initialView).toBe('homeView')
+      
+    })
+
+    it('views should be an object', () => {
+
+      expect(zumly0.views).toMatchObject({homeView})
+      
+    })
 
   })
+
+  describe('that are optional', () => {
+
+    it('cover should be a string', () => {
+
+      expect(zumly0.cover).toBe('width')
+      
+    })
+
+    it('duration should be a string', () => {
+
+      expect(zumly0.duration).toBe('1s')
+      
+    })
+
+    it('ease should be a string', () => {
+
+      expect(zumly0.ease).toBe('ease-in-out')
+      
+    })
+
+    it('effects should be an array. default', () => {
+
+      expect(zumly0.effects).toEqual(expect.arrayContaining(['none','none']))
+      
+    })
+
+    it('effects should be an array. custom', () => {
+
+      expect(zumly1.effects).toEqual(expect.arrayContaining(['blur(0px) sepia(0) saturate(0) ', 'blur(0.8px) sepia(5) saturate(8) ']))
+      
+    })
+
+    it('debug should be a boolean', () => {
+
+      expect(zumly0.debug).toBe(false)
+      
+    })
+
+  })
+
 })
+
+describe('Zumly instances', () => {
+
+  it('first instance should be 1', () => {
+
+    expect(zumly0.instance).toBe(1)
+
+  })
+
+  it('second instance should be 2', () => {
+
+    expect(zumly1.instance).toBe(2)
+    
+  })
+
+  it('should be reflected on DOM', () => {
+
+    expect(document.body).toMatchSnapshot()
+    
+  })
+
+})
+  
+ 
